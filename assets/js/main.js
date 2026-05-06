7ded7733fabe3f71d617089536b622fe658a0ee0
bturkan <bayramovat2005@gmail.com>
2026-05-06 15:56:48 +0400

    Add staggered image animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,7 @@ gsap.from('.home__img-1', 5, {opacity:0, x:400})
 gsap.from('.home__img-6', 1.5, {opacity:0, y:200, delay: .1,ease:'back.out(1.5)'})
 gsap.from('.home__img-3', 1.5, {opacity:0, y:400, delay: .3,ease:'back.out(1.5)'})
 gsap.from('.home__img-5', 1.5, {opacity:0, y:400, delay: .5,ease:'back.out(1.5)'})
+gsap.from('.home__img-2', 1.5, {opacity:0, y:400, delay: .8,ease:'back.out(1.5)'})
 
 /*=============== SAKURA ANIMATION ===============*/
 
