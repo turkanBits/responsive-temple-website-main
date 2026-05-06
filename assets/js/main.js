edcde5ec7b1f45dad3d010ce0fdf3b10a693b16d
bturkan <bayramovat2005@gmail.com>
2026-05-06 16:01:26 +0400

    Animate

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,6 +44,10 @@ gsap.from('.home__img-6', 1.5, {opacity:0, y:200, delay: .1,ease:'back.out(1.5)'
 gsap.from('.home__img-3', 1.5, {opacity:0, y:400, delay: .3,ease:'back.out(1.5)'})
 gsap.from('.home__img-5', 1.5, {opacity:0, y:400, delay: .5,ease:'back.out(1.5)'})
 gsap.from('.home__img-2', 1.5, {opacity:0, y:400, delay: .8,ease:'back.out(1.5)'})
+gsap.from('.home__img-4', 1.5, {opacity:0, y:200, delay: 1.8,ease:'back.out(1.5)'})
+gsap.from('.home__data', 1.5, {opacity:0, y:-100, delay: 2.5,ease:'back.out(1.5)'})
+gsap.from('.home__lantern-1', 1.5, {opacity:0, x:70, delay:3,ease:'back.out(1.5)'})
+gsap.from('.home__lantern-2', 1.5, {opacity:0, x:-70, delay:3.5,ease:'back.out(1.5)'})
 
 /*=============== SAKURA ANIMATION ===============*/
 
